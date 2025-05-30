e67fb8464bdb925681d56dd6583e11787b278a1e
chiran <482020095@qq.com>
2025-05-30 20:06:25 +0800

    add frontend 5-30

---
+++ owlpot-common/src/main/java/com/owlpot/OwlpotCommonApplication.java
@@ -0,0 +1,13 @@
+package com.owlpot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OwlpotCommonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OwlpotCommonApplication.class, args);
+    }
+
+}
---
+++ owlpot-server/src/main/java/com/owlpot/OwlpotServerApplication.java
@@ -0,0 +1,13 @@
+package com.owlpot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OwlpotServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OwlpotServerApplication.class, args);
+    }
+
+}
